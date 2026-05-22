2d83f82a58661a9c84ff4a894b61bb559f858592
Jozefgabriel16 <jozefgabriel16@gmail.com>
2026-05-22 11:50:07 -0400

    nuevo

---
+++ src/lib/components/Alfajor.svelte
@@ -0,0 +1,12 @@
+<script>
+    const { precio, titulo, img } = $props();
+</script>
+
+<div>
+    <p>{titulo}</p>
+    <p>{precio}$</p>
+
+    <div>
+        <img src={img} alt="" />
+    </div>
+</div>
--- src/routes/bakery/+page.svelte
+++ src/routes/bakery/+page.svelte
@@ -1,7 +1,19 @@
+<script>
+    import Alfajor from "$lib/components/Alfajor.svelte";
+</script>
+
 <section>
+    <p>La Collection</p>
+    <p>Pastelería boutique</p>
+    <p>
+        A curated symphony off French tradition and modern art with a
+        sensasional ingredients
+    </p>
+</section>
 
-<p>La Collection</p>
-<p>Pastelería boutique</p>
-<p>A curated symphony off French tradition and modern art with a sensasional ingredients</p>
-
+<section>
+    <Alfajor precio={5.0} titulo="Alfajor de chocolate" img="/alfajorchocolate.jpg" />
+    <Alfajor precio={2.0} titulo="Alfajor de pistacho" img="/alfajor.jpg" />
+    <Alfajor precio={3.0} titulo="Alfajor de crema" img="/alfajora.jpg" />
+    <Alfajor precio={4.0} titulo="Alfajor de fresa" img="/alfajorb.jpg" />
 </section>
