c6fb0ecd38829962d25e82a9ac3f159eb850317f
Jozefgabriel16 <jozefgabriel16@gmail.com>
2026-05-29 10:28:09 -0400

    menu listo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,19 +9,19 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <nav class="flex justify-between items-center px-10 py-3">
-	<p class="text-xl font-extrabold flex gap-0.5 items-center">
+	<p class="text-3xl font-extrabold font-serif flex gap-0.5 items-center">
 		<Icon icon="mdi:bakery" width={60} height={60} />
-		Panaderia Las Delicias 
+		Panaderia <br> Las Delicias 
 	</p>
 
-	<div class="flex gap-3.5">
+	<div class="flex gap-3.5 uppercase font-semibold">
 		<a class="flex gap-0.5 items-center" href="/">Home</a>
-		<a class="flex gap-0.5 items-center" href="/breads">Panes <Icon icon="mdi:bread" /></a>
-		<a class="flex gap-0.5 items-center" href="/bakery">Pastelería <Icon icon="mdi:chocolate" /></a>
-		<a class="flex gap-0.5 items-center" href="/breakfast">Desayunos <Icon icon="fluent-mdl2:breakfast" /></a>
+		<a class="flex gap-0.5 items-center" href="/breads">Panes</a>
+		<a class="flex gap-0.5 items-center" href="/bakery">Pastelería</a>
+		<a class="flex gap-0.5 items-center" href="/breakfast">Desayunos</a>
 	</div>
  
-	<button class="font-extrabold text-amber-50 px-7 py-1.5 bg-stone-600">Visit us</button>
+	<button class="font-extrabold uppercase text-amber-50 px-10 py-2 bg-stone-600">Visit us</button>
 </nav>
 
 {@render children()}
