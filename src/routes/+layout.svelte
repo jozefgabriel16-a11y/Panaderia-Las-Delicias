4d2f8e6293c8e63d5508fb4d541cb182e7321a29
Jozefgabriel16 <jozefgabriel16@gmail.com>
2026-06-17 10:37:52 -0400

    dd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,11 +24,6 @@
 			>Desayunos y Cafe</a
 		>
 	</div>
-
-	<button
-		class="font-extrabold uppercase text-amber-50 px-10 py-2 bg-stone-600 hover:bg-stone-700 transition-colors"
-		>Visit us</button
-	>
 </nav>
 
 {@render children()}
